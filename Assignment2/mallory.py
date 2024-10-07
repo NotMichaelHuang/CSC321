9d076b7f71c63eecf3d119318fbec815ac14342c
Michael Huang <94334792+MHFLCMAY2UCSD@users.noreply.github.com>
2024-10-07 01:05:41 -0700

    Initial Push

---
+++ Assignment2/mallory.py
@@ -0,0 +1,15 @@
+def Mallory():
+    def __init__(self, q: int):
+        self.alice = None
+        self.bob = None
+        self.sleeper = q
+    
+    
+    def set_alice_pub(self, alice_pub: int) -> None:
+        self.alice = alice_pub
+
+    
+    def set_bob_pub(self, bob_pub: int) -> None:
+        self.bob = bob_pub
+    
+    
